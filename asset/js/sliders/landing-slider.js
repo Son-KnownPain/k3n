e210e05df799522c0c629542447bf4e3b5cee580
Son-KnownPain <sonhong443@gmail.com>
2024-06-05 20:43:36 +0700

    first commit

---
+++ asset/js/sliders/landing-slider.js
@@ -0,0 +1,50 @@
+// Partners slider implement
+$(document).ready(function() {
+    const slider = $('#partners-slider');
+
+    slider.slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        prevArrow: false,
+        nextArrow: false,
+        arrows: false,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 740,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+        ],
+    })
+
+    // Features slider implement
+    const features = $('#features-slider');
+
+    features.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: false,
+        nextArrow: false,
+        arrows: false,
+        dots: true,
+        centerMode: true,
+    })
+
+    const bsContainerWidth = document.querySelector('.container').offsetWidth;
+    const myScreenWidth = document.body.offsetWidth;
+    const featuresContainerMarginLeft = (myScreenWidth - bsContainerWidth) / 2;
+
+    document.querySelector('#features-container .slick-dots').style.marginLeft = - featuresContainerMarginLeft + 'px'
+
+    document.getElementById('features-container').style.width = bsContainerWidth + featuresContainerMarginLeft + 'px'
+
+    document.querySelectorAll('#features-container .slick-dots li button').forEach(elm => elm.innerHTML = ``)
+})
